62fcf5d6b740a9c8332f093543969273b4dc0703
M-Fotheringham <michaelf2323@gmail.com>
2025-03-25 16:25:02 -0400

    wide sweeping updates

--- src/SimuSamp/new_funcs/compute/compute_hpfs.py
+++ src/SimuSamp/new_funcs/compute/compute_hpfs.py
@@ -4,12 +4,8 @@ import geopandas as gpd
 
 
 def compute_hpfs(
-        sampleid,
-        cells,
-        annos,
-        width_microns,
-        microns_per_pixel=0.22715
-        ):
+    sampleid, cells, annos, width_microns, microns_per_pixel=0.22715
+):
     """
     Generate a partition of high-power fields and compute cell counts.
     """
@@ -19,7 +15,7 @@ def compute_hpfs(
     mm2_per_pixels2 = (microns_per_pixel / 1000) ** 2
     partition = annos[annos["layer"] == "partition"]["geometry"].iloc[0]
     tumour = annos[annos["layer"] == "tumour"]["geometry"].iloc[0]
-    im = annos[annos["layer"] == "IM"]["geometry"].iloc[0]
+    im = annos[annos["layer"] == "outer_IM"]["geometry"].iloc[0]
 
     # Generate HPFs from annotation bounds
     min_x, min_y, max_x, max_y = partition.bounds
@@ -33,8 +29,13 @@ def compute_hpfs(
         for y in range(len(py) - 1):
 
             poly_hpf = Polygon(
-                [[px[x], py[y]], [px[x], py[y + 1]],
-                 [px[x + 1], py[y + 1]], [px[x + 1], py[y]]])
+                [
+                    [px[x], py[y]],
+                    [px[x], py[y + 1]],
+                    [px[x + 1], py[y + 1]],
+                    [px[x + 1], py[y]],
+                ]
+            )
 
             hpf_inter = poly_hpf.intersection(partition)
             hpf_area = hpf_inter.area * mm2_per_pixels2
@@ -46,9 +47,12 @@ def compute_hpfs(
                 cell_den = cell_count / hpf_area
 
                 # Estimate HPF region from layer area
-                tum_frac = poly_hpf.intersection(tumour).area
-                im_frac = poly_hpf.intersection(im).area
-                if tum_frac > im_frac:
+                tum_frac = hpf_inter.intersection(tumour).area / poly_hpf.area
+                im_frac = hpf_inter.intersection(im).area / poly_hpf.area
+                # (IM depth * core diameter) / core area
+                # 200 * width_microns / width_microns ** 2
+                im_min_frac = 200 / width_microns
+                if (tum_frac > im_frac) & (im_frac < im_min_frac):
                     region = "tumour"
                 else:
                     region = "IM"
@@ -61,9 +65,9 @@ def compute_hpfs(
                         "geometry": poly_hpf,
                         "density": cell_den,
                         "cell_count": cell_count,
-                        "area": hpf_area
-                        }
-                        )
+                        "area": hpf_area,
+                    }
+                )
 
     computed_hpfs = gpd.GeoDataFrame(computed_hpfs, geometry="geometry")
 
