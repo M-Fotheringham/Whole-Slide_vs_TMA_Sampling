3314d9a25a86c5dc1e3d0f7e2bdfaca7bd39749d
M-Fotheringham <michaelf2323@gmail.com>
2025-12-31 11:26:17 -0500

    TMA NN dists

--- src/SimuSamp/functions/compute/n_neighbours.py
+++ src/SimuSamp/functions/compute/n_neighbours.py
@@ -2,7 +2,24 @@ import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
 
-def neighbours(gdf, k, microns_per_pixel=0.22715):
+def edge_correction_weight(point, core, distance):
+    # Get fraction of the buffer within the analysis area
+    circle = point.buffer(distance)
+    intersected_area = circle.intersection(core).area
+
+    weight = circle.area / intersected_area
+
+    return weight
+
+
+def neighbours(
+    gdf,
+    k,
+    microns_per_pixel=0.22715,
+    edge_correction=False,
+    core=None,
+    distance=None,
+):
     """
     Compute the nearest neighbour distances for a GeoDataFrame.
     Args:
@@ -11,15 +28,30 @@ def neighbours(gdf, k, microns_per_pixel=0.22715):
     """
     if len(gdf) < k + 1:
         return np.nan
-    
+
     # Get the coordinates of the input GeoDataFrame
     xy = np.array(gdf.geometry.apply(lambda geom: (geom.x, geom.y)).tolist())
 
     # Compute the distance from each point to n nearest neighbours
-    nn = NearestNeighbors(n_neighbors=k+1).fit(xy)
+    nn = NearestNeighbors(n_neighbors=k + 1).fit(xy)
     distances, _ = nn.kneighbors(xy)
 
     dataset_distances = distances[:, 1:]
+
+    if edge_correction:
+        if core is None or distance is None:
+            raise ValueError(
+                "Core area and radius must be provided for edge correction."
+            )
+
+        weights = []
+        for p in gdf.geometry:
+            weights.append(edge_correction_weight(p, core, distance))
+
+        weights = np.array(weights)
+
+        scale = 1.0 / np.sqrt(weights)
+        dataset_distances = dataset_distances * scale[:, None]
 
     dataset_distances = dataset_distances * microns_per_pixel
 
